48805365b17dad1b5a6dbe3f053d6167914d39d5
Juan Carlos López <cr1757579@gmail.com>
2026-05-13 23:07:04 -0600

    Practica ASP.NET 13/05/2026

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,46 @@
+﻿using IDGS904ASP.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace IDGS904ASP.Controllers
+{
+    public class HomeController : Controller
+    {
+        public RedirectToRouteResult Index()
+        {
+            return RedirectToAction("Index", "Pruebas");
+        }
+
+        //public RedirectResult Index()
+        //{
+        //    return Redirect("https://google.com.mx");
+        //}
+
+        //public ActionResult Index()
+        //{
+        //    var alumno1 = new Persona() { Nombre = "Carlos", Edad = 21, Empleado = true };
+        //    var alumno2 = new Persona() { Nombre = "Oscar", Edad = 21, Empleado = false };
+
+        //    //return View();
+        //    return Json(new List<Persona>() { alumno1, alumno2 }, JsonRequestBehavior.AllowGet);
+
+        //}
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ Controllers/PruebasController.cs
@@ -0,0 +1,39 @@
+﻿using IDGS904ASP.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace IDGS904ASP.Controllers
+{
+    public class PruebasController : Controller
+    {
+        // GET: Pruebas
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Contacto()
+        {
+            ViewBag.Nombre = "Juan Carlos López Ruiz";
+            ViewBag.Titulo = "IDGS";
+            ViewData["Materia"] = "DWI";
+            return View();
+        }
+
+        public ActionResult Empleado()
+        {
+            var persona = new Persona()
+            {
+                Nombre = "Carlos",
+                Edad = 21,
+                Empleado = false,
+                Nacimiento = new DateTime(2005, 1, 1)
+            };
+            ViewBag.Propiedades = persona;
+            return View();
+        }
+    }
+}
